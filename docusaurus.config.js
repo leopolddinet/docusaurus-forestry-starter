abcbc2a971b7d94d09cf4783201c02f78e62d10e
leopolddinet <66524636+leopolddinet@users.noreply.github.com>
2020-06-14 19:56:32 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ module.exports = {
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'LDTI',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo LDTI.jpg',
