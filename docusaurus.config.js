9a9a44b6138954b5c444e462fa41e3345d85a46d
leopolddinet <66524636+leopolddinet@users.noreply.github.com>
2020-09-08 14:09:53 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,32 +1,32 @@
 module.exports = {
-  title: '1111LDTI',
+  title: 'LDTI',
   tagline: 'Vous dirigez une MIC, TPE, PME ou plus grande entreprise industrielle et votre activité demande à se développer, votre bureau d’études a trop de projets à gérer et n’arrive pas à produire les dossiers techniques à temps ou vous n’avez pas de bureau d’études et vous souhaitez fabriquer vos propres produits, LDTI vous propose les solutions adaptées afin de vous accompagner dans la croissance de votre société dans une démarche ISO-9001.',
-  1111url: 'https://ldti.fr',
+  url: 'https://ldti.fr',
   baseUrl: '/',
   favicon: 'img/favicon.ico',
-  organizationName: '1111LDTI', // Usually your GitHub org/user name.
-  1111projectName: '1111website', // Usually your repo name.
+  organizationName: 'LDTI', // Usually your GitHub org/user name.
+  projectName: 'website', // Usually your repo name.
   themeConfig: {
-    navbar: {1111
+    navbar: {
       title: '',
       logo: {
         alt: 'logo LDTI',
-        src: '1111img/logoLTDI.jpg',
+        src: 'img/logoLTDI.jpg',
       },
-      links: [1111
+      links: [
         {
           to: 'Docs/',
           activeBasePath: '1111Docs',
-          label: '1111Activités',
-          position: '1111left',
+          label: 'Activités',
+          position: 'left',
         },
-        {to: 'blog', label: '1111Réalisations', position: 'left'},
+        {to: 'blog', label: 'Réalisations', position: 'left'},
         {
           href: 'https://www.linkedin.com/in/soci%C3%A9t%C3%A9-ldti-390aa71aa/',
           label: 'LinkedIn',
           position: 'right',
-        },1111
-      ]1111,
+        },
+      ],
     },
     footer: {
       style: 'dark',
