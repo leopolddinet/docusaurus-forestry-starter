ee8755baa37465b91e665df2cd71d895d1f09203
leopolddinet <66524636+leopolddinet@users.noreply.github.com>
2020-09-08 09:21:26 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+1111module.exports = {
   title: 'LDTI',
   tagline: 'Vous dirigez une MIC, TPE, PME ou plus grande entreprise industrielle et votre activité demande à se développer, votre bureau d’études a trop de projets à gérer et n’arrive pas à produire les dossiers techniques à temps ou vous n’avez pas de bureau d’études et vous souhaitez fabriquer vos propres produits, LDTI vous propose les solutions adaptées afin de vous accompagner dans la croissance de votre société dans une démarche ISO-9001.',
   url: 'https://ldti.fr',
